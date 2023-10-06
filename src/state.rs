503ff913da8d67e129205bb63e935c0784a3e72b
Dr. Kiji <merdan.jp@gmail.com>
2023-10-06 16:34:36 +0900

    observation state

---
+++ src/state.rs
@@ -0,0 +1,46 @@
+
+#[derive(Debug, Copy, Clone)]
+pub struct CarState {
+    pub x: f64,
+    pub y: f64,
+    pub yaw: f64,
+    pub velocity: f64,
+}
+
+//implement the addition and subtraction for car_state
+impl std::ops::Add for CarState {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            yaw: self.yaw + other.yaw,
+            velocity: self.velocity + other.velocity,
+        }
+    }
+}
+
+impl std::ops::Sub for CarState {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            yaw: self.yaw - other.yaw,
+            velocity: self.velocity - other.velocity,
+        }
+    }
+}
+
+//implement the multiplication for car_state
+impl std::ops::Mul<f64> for CarState {
+    type Output = Self;
+    fn mul(self, other: f64) -> Self {
+        Self {
+            x: self.x * other,
+            y: self.y * other,
+            yaw: self.yaw * other,
+            velocity: self.velocity * other,
+        }
+    }
+}
